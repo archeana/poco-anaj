e644b83b104a2dc2bda73849fa302a32d3ab423c
archeana <anaclaudiasousajesus6@gmail.com>
2024-04-24 05:49:39 +0200

    Js exercises

---
+++ week04/age calculator/age.js
@@ -0,0 +1,10 @@
+// you could also do like prompt but I was lazy
+
+
+var birthYear = 1992;
+var futureYear = 2030;
+
+
+var age = futureYear - birthYear;
+
+console.log("I will be either " + (age-1) + " or " + age + " in " + futureYear );
